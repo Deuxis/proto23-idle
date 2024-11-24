0c83a678bbc4dd1c0f957331c3529cc8b30c685a
Andrzej Korwin-Mikke <andrzej.kmikke@gmail.com>
2024-11-24 17:46:40 +0100

    Simplify loadingScreen

--- game/ui.mjs
+++ game/ui.mjs
@@ -1,14 +1,14 @@
 const root = document.body
 
-const loadingScreen = (() => {
+const genLoadingScreen = () => {
 	const loading = document.createElement('div')
 	loading.className = 'loadingContainer'
 	const loadingSpan = document.createElement('span')
 	loadingSpan.appendChild(document.createTextNode('LOADING'))
 	loading.appendChild(loadingSpan)
 	return loading
-})()
+}
 
 export const init = () => {
-	root.replaceChildren(loadingScreen)
+	root.replaceChildren(genLoadingScreen())
 }
